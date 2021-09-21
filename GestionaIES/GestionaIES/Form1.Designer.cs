9632c16324715e06ddde6bee80ee7c9a62647d08
carlosgutfer <carlos.gutfer@hotmail.es>
2021-09-21 16:56:50 +0200

    primer commit

---
+++ GestionaIES/GestionaIES/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace GestionaIES
+{
+    partial class GestionaIES
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestionaIES));
+            this.bt_aceptar = new System.Windows.Forms.Button();
+            this.bt_cancelar = new System.Windows.Forms.Button();
+            this.linklabel_web = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.lbl_clave = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.lbl_error = new System.Windows.Forms.Label();
+            this.lbl_responsability = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_aceptar
+            // 
+            resources.ApplyResources(this.bt_aceptar, "bt_aceptar");
+            this.bt_aceptar.Name = "bt_aceptar";
+            this.bt_aceptar.UseVisualStyleBackColor = true;
+            this.bt_aceptar.Click += new System.EventHandler(this.bt_aceptar_Click);
+            // 
+            // bt_cancelar
+            // 
+            resources.ApplyResources(this.bt_cancelar, "bt_cancelar");
+            this.bt_cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_cancelar.Name = "bt_cancelar";
+            this.bt_cancelar.UseVisualStyleBackColor = true;
+            this.bt_cancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linklabel_web
+            // 
+            resources.ApplyResources(this.linklabel_web, "linklabel_web");
+            this.linklabel_web.Name = "linklabel_web";
+            this.linklabel_web.TabStop = true;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_usuario
+            // 
+            resources.ApplyResources(this.lbl_usuario, "lbl_usuario");
+            this.lbl_usuario.Name = "lbl_usuario";
+            // 
+            // lbl_clave
+            // 
+            resources.ApplyResources(this.lbl_clave, "lbl_clave");
+            this.lbl_clave.Name = "lbl_clave";
+            // 
+            // txt_user
+            // 
+            resources.ApplyResources(this.txt_user, "txt_user");
+            this.txt_user.Name = "txt_user";
+            this.txt_user.TextChanged += new System.EventHandler(this.txt_user_TextChanged);
+            // 
+            // txt_password
+            // 
+            resources.ApplyResources(this.txt_password, "txt_password");
+            this.txt_password.Name = "txt_password";
+            // 
+            // lbl_error
+            // 
+            resources.ApplyResources(this.lbl_error, "lbl_error");
+            this.lbl_error.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error.Name = "lbl_error";
+            this.lbl_error.UseWaitCursor = true;
+            // 
+            // lbl_responsability
+            // 
+            resources.ApplyResources(this.lbl_responsability, "lbl_responsability");
+            this.lbl_responsability.Name = "lbl_responsability";
+            // 
+            // GestionaIES
+            // 
+            this.AcceptButton = this.bt_aceptar;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.bt_cancelar;
+            this.Controls.Add(this.lbl_responsability);
+            this.Controls.Add(this.lbl_error);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.lbl_clave);
+            this.Controls.Add(this.lbl_usuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linklabel_web);
+            this.Controls.Add(this.bt_cancelar);
+            this.Controls.Add(this.bt_aceptar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GestionaIES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_aceptar;
+        private System.Windows.Forms.Button bt_cancelar;
+        private System.Windows.Forms.LinkLabel linklabel_web;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.Label lbl_clave;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label lbl_error;
+        private System.Windows.Forms.Label lbl_responsability;
+    }
+}
+
---
+++ GestionaIES/GestionaIES/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionaIES
+{
+    public partial class GestionaIES : Form
+    {
+        public GestionaIES()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has pulsado Cancelar");
+        }
+
+        private void bt_aceptar_Click(object sender, EventArgs e)
+        {
+
+            String user = txt_user.Text;
+            String password = txt_password.Text;
+
+            if ((user == "1234") && (password == "1234"))
+            {
+                Form2 new_form = new Form2();
+                this.Close();
+
+                new_form.Show();
+            }
+
+            /*
+            Label new_label = new Label();
+            new_label.Text = "Prueba";
+            new_label.Location = new System.Drawing.Point(500, 25);
+            new_label.Size = new System.Drawing.Size(70, 20);
+            this.Controls.Add(new_label);*/
+        }
+
+        private void txt_user_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
